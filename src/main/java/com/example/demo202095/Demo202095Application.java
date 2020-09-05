784b07d0150feb624756393c826697a23a3933a5
zgzzjgw <zzlgjgw@163.com>
2020-09-05 16:54:22 +0800

    test

---
+++ src/main/java/com/example/demo202095/Demo202095Application.java
@@ -0,0 +1,13 @@
+package com.example.demo202095;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo202095Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo202095Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo202095/Demo202095ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo202095;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo202095ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
